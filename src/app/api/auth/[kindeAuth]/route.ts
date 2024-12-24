0e775cb6f84572540522349a8b931b96f5359937
Mher <mhergharibyan550@gmail.com>
2024-12-24 15:14:30 +0400

    Initial commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,18 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 export const GET = handleAuth();
+
+export const OPTIONS = (req: NextRequest) => {
+  const res = NextResponse.next();
+
+  if (req.method === "OPTIONS") {
+    res.headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.headers.set("Access-Control-Allow-Origin", "*");
+
+    return res;
+  }
+};
