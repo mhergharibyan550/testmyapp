ea7783c88d1890758817f9357cfeafe66e67c979
Mher <mhergharibyan550@gmail.com>
2024-12-24 18:10:15 +0400

    Check

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, rsc",
 };
 
 export function middleware(req: NextRequest) {
