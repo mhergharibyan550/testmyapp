39bf1d3bf7ed4738c2cdb18224faeb0d5b8382f7
Mher <mhergharibyan550@gmail.com>
2024-12-24 17:46:35 +0400

    Check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  req.headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  req.headers.set(
+  const res = NextResponse.next();
+  res.headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.headers.set(
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization"
   );
-  req.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Origin", "*");
 
-  return req;
+  return res;
 }
